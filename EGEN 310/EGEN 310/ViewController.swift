ab603cfb3569143145b5fb8c1986212a3853f8f6
Kyle Webster <kyle1234webster@gmail.com>
2019-04-24 22:10:45 -0600

    Update ViewController.swift

--- EGEN 310/EGEN 310/ViewController.swift
+++ EGEN 310/EGEN 310/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     
     //Instantiate CocoaMQTT as mqttClient
     let mqttClient = CocoaMQTT(clientID: "iOS Device", host: "169.254.213.44", port: 1883)
+    private var current = 3
     
+    //switch to enable and disable forward enable or disable
     @IBAction func forwardSW(_ sender: UISwitch) {
         if sender.isOn {
             mqttClient.publish("rpi/gpio", withString: "for on")
@@ -25,20 +27,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var angle: UILabel!
-    
+    //Slider to control speed, is not continuous update due to Raspberry Pi CPU and RAM memory
     @IBAction func speedSL(_ sender: UISlider) {
         let currentValue = Int(sender.value)
         label.text = "Speed: \(currentValue)"
         debugPrint(currentValue)
         mqttClient.publish("rpi/speed", withString: "\(currentValue)")
     }
-    @IBAction func steeringSL(_ sender: UISlider) {
-        let currentValue = Int(sender.value)
-        angle.text = "Angle: \(currentValue)"
-        debugPrint("Angle value: \(currentValue)")
-        mqttClient.publish("rpi/servo", withString: "\(currentValue)")
-    }
     
+    //Slider to control the steering, continuous update with value ranges of 1-5 where 1 is left and 5 is right
+    @IBAction func steeringSL(_ sender: UISlider) {
+        let val = Int(sender.value)
+        angle.text = "Angle \(val)"
+        if val != current{
+            if val == 1{
+                debugPrint("1")
+                mqttClient.publish("rpi/servo", withString: "1")
+            }else if val == 2{
+                debugPrint("2")
+                mqttClient.publish("rpi/servo", withString: "2")
+            }else if val == 3{
+                debugPrint("3")
+                mqttClient.publish("rpi/servo", withString: "3")
+            }else if val == 4{
+                debugPrint("4")
+                mqttClient.publish("rpi/servo", withString: "4")
+            }else{
+                debugPrint("5")
+                mqttClient.publish("rpi/servo", withString: "5")
+        }
+        current = val
+    }
+    }
+    //switch to enable backwards drive either on or off
     @IBAction func backwardSW(_ sender: UISwitch) {
         if sender.isOn{
             mqttClient.publish("rpi/gpio", withString: "back on")
@@ -67,4 +88,3 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 }
-
